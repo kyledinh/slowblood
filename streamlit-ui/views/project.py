17eb2aa4bcbdf37172a3b22b88136cd55a191324
Kyle Dinh <kyledinh@gmail.com>
2024-02-19 15:56:07 -0800

    download buttons

--- streamlit-ui/views/project.py
+++ streamlit-ui/views/project.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 import streamlit as st
 from app.session import get_sess, get_sess_or_blank, get_sess_or_undefined, set_sess
-from app.config import const as kon
+from app.config import const 
 import slowblood
 
 
@@ -18,7 +18,30 @@ class ProjectView:
 
         st.header('Current Project:')
         with st.container():
-            placeholder = get_sess_or_undefined(kon.PROJECT_ID)
+            placeholder = get_sess_or_undefined(const.PROJECT_ID)
             pid = st.text_input("Project ID:", placeholder)
             if pid:
-                set_sess(kon.PROJECT_ID, pid) 
+                set_sess(const.PROJECT_ID, pid) 
+
+        if get_sess(const.JSON_FILE) is not None:
+            with st.expander("View JSON Data"):
+                json_data = get_sess_or_undefined(const.JSON_DATA)
+                st.download_button(
+                    label="Download JSON file",
+                    data=json_data,
+                    file_name=get_sess(const.JSON_FILE),
+                    mime='text/json',
+                )
+                st.json(json_data)
+
+
+        if get_sess(const.CSV_FILE) is not None:
+            with st.expander("View CSV Data"):
+                csv_df = pd.read_csv(get_sess(const.CSV_FILE))
+                st.download_button(
+                    label="Download CSV file",
+                    data=csv_df.to_csv().encode('utf-8'),
+                    file_name=get_sess(const.CSV_FILE),
+                    mime='text/csv',
+                )
+                st.write(csv_df)
